d5b7ce198d4e085e2544485a513d3abf462018f5
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-15 15:28:34 -0300

    Trabalhando com validações do formulario

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -18,12 +18,17 @@ export class ContatoComponent implements OnInit {
   ngOnInit(): void {
     this.formulario = this.fb.group({
       nome: ['', Validators.required],
-      email: ['', Validators.email]
+      email: ['', [Validators.required, Validators.email]]
     })
   }
 
   submit() {
-    console.log(this.formulario?.value)
+    const erroNomeRequired = this.formulario.controls.nome.errors?.required
+    const erroEmailInvalido = this.formulario.controls.email.errors?.email
+
+    console.log('erroNomeRequired: ', erroNomeRequired)
+    console.log('erroEmailInvalido: ', erroEmailInvalido)
+
     // this.service.save(c).subscribe(resposta => {
     //   console.log(resposta)
     // })
